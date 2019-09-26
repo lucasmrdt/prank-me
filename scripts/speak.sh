858479c1cdb392160c2e87aa3456d36152629457
Lucas Marandat <lucas.mrdt@gmail.com>
2019-09-26 17:55:56 +0200

    improve script

--- scripts/speak.sh
+++ scripts/speak.sh
@@ -3,16 +3,22 @@
 cd /tmp
 mkfifo FIFO > /dev/null 2>&1
 
+say() {
+  amixer set Master 100%+ > /dev/null 2>&1
+  espeak -v fr "ducrot" > /dev/null 2>&1
+  espeak -v fr "grosse" > /dev/null 2>&1
+  espeak -v fr "mer2" > /dev/null 2>&1
+  amixer set Master 0%- > /dev/null 2>&1
+}
+
 start() {
   while [ 1 ]; do
+    say
     xrandr -o inverted
-    amixer set Master 100%+ > /dev/null 2>&1
-    espeak -v fr "ducrot" > /dev/null 2>&1
-    espeak -v fr "grosse" > /dev/null 2>&1
-    espeak -v fr "mer2" > /dev/null 2>&1
-    amixer set Master 0%- > /dev/null 2>&1
-    sleep 1
+    sleep 0.5
+    say
     xrandr -o normal
+    sleep 0.5
   done;
 }
 
